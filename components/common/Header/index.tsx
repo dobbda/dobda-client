b44cbca880ed1d30a7b4dbaffeba2da29b21640b
catca <57560457+catca@users.noreply.github.com>
2022-05-22 12:12:50 +0900

    feat: styled-components 추가

---
+++ components/common/Header/index.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import styled from "styled-components";
+
+const Header: FC = () => {
+  return <Container>QQnAA</Container>;
+};
+
+export default Header;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
