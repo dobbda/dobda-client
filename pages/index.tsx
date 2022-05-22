4f333da4a28ca7cf52221dce681d71923abdc032
catca <57560457+catca@users.noreply.github.com>
2022-05-22 14:34:48 +0900

    style: prettier 적용
    npm run format
    npm run format:fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Header from "../components/common/Header";
-import styles from "../styles/Home.module.css";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Header from 'components/common/Header';
+import styles from 'styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
