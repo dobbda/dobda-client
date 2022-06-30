f2d1ed3c09b2feba7f2a9a1f135a625e01655b2e
Developshin <shin.github@gmail.com>
2022-06-30 16:37:00 +0900

    feat: userInof popup

---
+++ src/components/UserInfo/style/userInfo.Element.ts
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+export const Div = styled.div`
+
+  background-color: #fbf8f8;
+  border: solid 1.5px ${({ theme }) => theme.color.border};
+  border-radius: 4px;
+  min-height: 200px;
+  width: 280px;
+
+  align-items: center;
+  justify-content: center;
+`;
+export const P = styled.p`
+  font-size: 13px;
+  color: #828282;
+  text-align: center;
+  margin:0;
+`;
+
+
+export const BaseInfo = styled.div`
+  display: flex; 
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  width: 100%;
+  border-radius: 4px 4px 0 0;
+  background-color: #fff;;
+  padding:15px 0;
+  img {
+    border-radius: 100%;
+    color: #333333;
+  }
+
+  h3 {
+    font-size: 18px;
+    font-weight: bold;
+  }
+  .e-mail{
+    color: #52547E;
+    font-size: 15px;
+  }
+`;
+
+
+export const UserActive = styled.div`
+  display: flex;
+  border-top: solid 1px #d6d6d6;
+  border-bottom: solid 1px #d6d6d6;
+  width: 100%;
+  justify-content: center;
+  text-align: center;
+`;
+
+export const Item = styled.div`
+  border: solid 1px #d6d6d6;
+  margin: -1px -0.5px;
+  padding: 5px 10px;
+
+  span,em{
+    font-size: 16px;
+    color: #52547E;
+  }
+`;
+
+export const UserTech = styled.div`
+  min-height: 50px;
+  text-align: center;
+  margin: 10px 0;
+div{
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+
+}
+
+`
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 import {MessageBox} from 'src/components/MessageBox/MessageBox';
 import {Popover} from 'src/components/common';
-
+import {UserInfo} from 'src/components/UserInfo'
 type Props = {}
 
 const Test:React.FC = (props: Props) => {
@@ -11,7 +11,9 @@ const Test:React.FC = (props: Props) => {
   }
   return (
     <>
-      <div><MessageBox /></div>
+          <div><MessageBox /></div>
+    <div style={{width:"300px",height:"300px", backgroundColor:"#0005", overflow: "hidden"}}>
+
       <Popover 
         visible={visible} // false 넘겨주면 모달 popover modal 닫힘
         content = {<><MessageBox/><button onClick={closed}>Close</button></>}
@@ -23,6 +25,8 @@ const Test:React.FC = (props: Props) => {
         <button onClick={()=>setVisible(true)}>popover event</button>
 
       </Popover>
+      </div>
+      <UserInfo/>
     </>
   )
 }
