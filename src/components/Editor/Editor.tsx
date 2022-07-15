e590f1beaf49ed48b5d17a86db63e92a38b28ec3
Developshin <shin.github@gmail.com>
2022-07-16 07:07:49 +0900

    feat: 에디터 업로드이미지 파일=> S3업로드 =>calback URL

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,7 +1,10 @@
-import React,{ useState, useEffect, useCallback,useRef} from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
-import { NextPage } from 'next';
+import AWS from 'aws-sdk';
+
+import {getUrl} from './getUrl'
+
 import { Editor as ToastEditor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 
@@ -26,51 +29,57 @@ interface Props {
   height: string;
 }
 
-const Editor: NextPage<Props> = ({ mdStr, setMdStr, onClickShow = false, height }) => {
+const Editor = ({ mdStr, setMdStr, onClickShow = false, height }: Props) => {
+  const fileName = Date.now(); // 이미지 이름
+  // 에디터 보여지는 핸들러
   const [showEditor, setShowEditor] = React.useState(onClickShow ? false : true);
   const onClickShowEditorHandler = useCallback(() => {
     setShowEditor(!showEditor);
+  }, [showEditor]);
 
-  },[showEditor]);
-  const editorRef = React.useRef<ToastEditor>(null);
+  const editorRef = useRef<ToastEditor>(null);
   // Editor Change 이벤트
 
   const onChangeEditor = () => {
-    if (editorRef.current) {
-      setMdStr(editorRef.current.getInstance().getMarkdown());
-    }
+    setMdStr(editorRef.current?.getInstance().getHTML() || '');
+    editorRef.current?.getInstance().removeHook('addImageBlobHook');
+    editorRef.current?.getInstance().addHook('addImageBlobHook', (blob, callback) => {
+      (async () => {
+        // const url = await getUrl(blob)
+        // callback(url,"설명")
+        
+         AWS.config.update({
+          region: process.env.NEXT_PUBLIC_REGION,
+          accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+        });
+
+        const upload = new AWS.S3.ManagedUpload({
+          params: {
+            Bucket: process.env.NEXT_PUBLIC_BUKET, // 버킷 이름
+            Key: fileName.toString() + '.png', // 유저 아이디
+            Body: blob, // 파일 객체
+          },
+        });
+
+        await upload.promise().then(
+          (res) => {
+
+            console.log('에딧이미지  ', res);
+            callback(decodeURI(res.Location), res.Key);
+          },
+          (err) => {
+            console.log(err);
+          });
+      })();
+      return false;
+    });
   };
-
-  useEffect(() => {
-    // editorRef.current.getInstance().setMarkdown(mdStr)
-    if (editorRef.current) {
-
-      // 전달받은 html값으로 초기화
-      editorRef.current.getInstance().setMarkdown(mdStr);
-
-      // 기존 이미지 업로드 기능 제거
-      editorRef.current.getInstance().removeHook('addImageBlobHook');
-      // 이미지 서버로 데이터를 전달하는 기능 추가
-      editorRef.current.getInstance().addHook('addImageBlobHook', (blob: any, callback: any) => {
-        (async () => {
-          const formData = new FormData();
-          formData.append('multipartFiles', blob);
-
-          const res = await axios.post('http://localhost:3000/uploadImage', formData);
-
-          callback(res.data, 'input alt text');
-        })();
-        return false;
-      });
-    }
-  }, []);
-
 
 
   const EditorElement = (
     <CustomReactQuill
-      
-      initialValue=' '
+      initialValue=" "
       previewStyle="vertical"
       initialEditType="wysiwyg"
       useCommandShortcut={false}
@@ -79,7 +88,7 @@ const Editor: NextPage<Props> = ({ mdStr, setMdStr, onClickShow = false, height 
       plugins={[colorSyntax, [codeSyntaxHighlight, { highlighter: Prism }]]}
       onChange={onChangeEditor}
       height={height}
-      language='ko-kr'
+      language="ko-kr"
       toolbarItems={[
         ['heading', 'bold', 'italic', 'strike'],
         ['hr', 'link'],
@@ -89,22 +98,18 @@ const Editor: NextPage<Props> = ({ mdStr, setMdStr, onClickShow = false, height 
       ]}
     />
   );
+
   return (
     <EditorStyle>
-
       {onClickShow && !showEditor && (
         <div onClick={onClickShowEditorHandler}>
           <ShowEditorBtn prefix={<ReplyIconS />} placeholder="답글 작성..." readOnly />
         </div>
       )}
 
-      {showEditor &&
-        EditorElement
-      }
+      {showEditor && EditorElement}
 
-    {onClickShow && showEditor &&
-      <CloseEditor onClick={()=>setShowEditor(false)} >Editor 접기</CloseEditor>
-    }
+      {onClickShow && showEditor && <CloseEditor onClick={() => setShowEditor(false)}>Editor 접기</CloseEditor>}
     </EditorStyle>
   );
 };
@@ -113,43 +118,40 @@ export default Editor;
 
 // style
 const CustomReactQuill = styled(ToastEditor)`
-  .toastui-editor-defaultUI{
+  .toastui-editor-defaultUI {
     border: 1px solid red !important;
-
   }
 `;
 
 const EditorStyle = styled.div`
   margin-top: 10px 0;
   position: relative;
-`
+`;
 
 const ReplyIconS = styled(ReplyIcon)`
   fill: gray;
   transform: rotate(180deg);
   height: 20px;
   width: 20px;
-  
 `;
 const ShowEditorBtn = styled(AntInput)`
   height: 50px;
   background-color: #fff;
   border-radius: 10px;
   /* border: px solid #465666; */
-  input{
+  input {
     /* background-color: #f5f5f5; */
   }
   input::placeholder {
     font-size: 15px;
     color: gray;
-  /* background-color: #f5f5f5; */
-
+    /* background-color: #f5f5f5; */
   }
 `;
 
 const CloseEditor = styled(Button)`
-position: absolute;
-bottom: 0;
-height: 29px;
-background-color: #f7f9fc;
-`
+  position: absolute;
+  bottom: 0;
+  height: 29px;
+  background-color: #f7f9fc;
+`;
