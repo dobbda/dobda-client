d118db8462adbc294928b3535aebe43ae22e5039
Developshin <shin.github@gmail.com>
2022-07-29 11:50:18 +0900

    chore: index side setting

---
+++ src/lib/cookieManager.ts
@@ -0,0 +1,12 @@
+
+import { NextApiRequestCookies } from 'next/dist/server/api-utils';
+import cookie from "cookie";
+
+
+
+export const cookieManager = async(req: any) => {
+
+const all = cookie.parse(req)
+
+return all;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,13 @@ import { useQueryClient } from 'react-query';
 import { Layout } from '../components/Layout';
 import { me, questions, users, requests } from 'src/store/DummyData';
 import { MainContent } from 'src/components/MainContent';
+import { NextPageContext } from 'next'
+import { GetServerSideProps } from 'next';
+import  axios  from 'axios';
+import { cookieManager } from 'src/lib/cookieManager';
+
+
+
 const Home: NextPage = () => {
   const queryClient = useQueryClient();
   useEffect(() => {
@@ -22,4 +29,12 @@ const Home: NextPage = () => {
   );
 };
 
+export const getServerSideProps: GetServerSideProps = async ({req, res}) => {
+			console.log( await cookieManager(req.headers.cookie))
+			return { props: {} }
+
+}
+
 export default Home;
+
+
