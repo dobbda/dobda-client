f2e108154a6ad1c7d5f2730f6ab2861ab1574e84
Developshin <shin.github@gmail.com>
2022-08-30 17:41:17 +0900

    소셜로그인 cdn_url 수정

--- src/components/SocialLogin/CDN_URL.ts
+++ src/components/SocialLogin/CDN_URL.ts
@@ -14,6 +14,11 @@ export const GOOGLE_URL = AUTHORIZE_URI + '?' + queryStr;
 
 export const GITHUB_URL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`;
 
-export const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_BASE_CLIENT_URL}&response_type=code`;
+export const KAKAO_URL = `
+https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}
+&redirect_uri=${process.env.NEXT_PUBLIC_BASE_CLIENT_URL}/login/callback/kakao&response_type=code`;
 
-export const NAVER_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_BASE_CLIENT_URL}/login/callback/kakao&state=test1`;
+export const NAVER_URL = `
+https://nid.naver.com/oauth2.0/authorize?response_type=code
+&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}
+&redirect_uri=${process.env.NEXT_PUBLIC_BASE_CLIENT_URL}/login/callback/naver&state=test1`;
