2fe736adb948f04a8825426625f0b6cd2d521ece
Developshin <shin.github@gmail.com>
2022-09-30 18:40:50 +0900

    메인화면 카테고리 에러 수정

--- src/lib/utils/localStorage.ts
+++ src/lib/utils/localStorage.ts
@@ -1,4 +1,4 @@
-export function setLocalStorage<T>(key: string, value: T) {
+export function setLocalStorage(key: string, value: any) {
   if (typeof window !== 'undefined' && typeof document !== 'undefined') {
     window.localStorage.setItem(key, Buffer.from(String(value), 'utf8').toString('base64'));
   }
@@ -7,11 +7,14 @@ export function setLocalStorage<T>(key: string, value: T) {
 export function getLocalStorage(key: string, obj?: boolean) {
   if (typeof window !== 'undefined' && typeof document !== 'undefined') {
     const objString = window.localStorage.getItem(key); // null 체크
-
-    if (obj) {
-      return JSON.parse(Buffer.from(String(objString), 'base64').toString('utf8'));
+    const decod = Buffer.from(String(objString), 'base64').toString('utf8');
+    if (objString && obj) {
+      return JSON.parse(decod);
+    } else if (objString) {
+      return decod;
+    } else {
+      return null;
     }
-    return Buffer.from(String(objString), 'base64').toString('utf8');
   }
 }
 
