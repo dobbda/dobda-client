1b3632683c41952c36ded53eae50a982a27a6cdf
Developshin <shin.github@gmail.com>
2022-12-15 15:58:40 +0900

    오타

--- src/components/banner/data.ts
+++ src/components/banner/data.ts
@@ -2,14 +2,14 @@ import { theme } from 'src/styles/Theme';
 
 export const bn = [
   {
-    title: '전문가에게 맡겨보세요',
+    title: 'title',
     sub: '___________________________・',
     buttonName: '소싱팀 모집하기',
     bg: theme.color.secondary,
     image: '/banner/banner_bg2.png',
   },
   {
-    title: 'a-z 혼자하기 힘드시죠?',
+    title: 'title',
     sub: '___________________________',
     buttonName: '프로젝트 의뢰하기',
     bg: theme.color.primary,
@@ -17,7 +17,7 @@ export const bn = [
     secondImg: '/banner/pr_bg1.svg',
   },
   {
-    title: '혼자 고민·말고 도움을 요청해봐요 🙋‍♂️ ',
+    title: 'title🙋‍♂️ ',
     sub: `___________________________`,
     buttonName: '질문하기',
     bg: theme.color.primary,
